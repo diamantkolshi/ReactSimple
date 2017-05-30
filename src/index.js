1c1a8f7cba7ac3910a7375888c1ecdf3e5823a04
Diamant <diamantkolshi@gmail.com>
2017-05-30 13:26:31 +0200

    add youtube api token and import SearchBar

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import SearchBar from './components/search_bar';
 // Create a new component. This component shuuld produce
 // some HTML
 
+const API_KEY = 'AIzaSyDg-2GI3OThgY9UETmMHw5yXKF1w0HxY9U';
+
 const App = () =>  {
-	return <div>Hi!</div>;
+	return (
+		<div>
+			<SearchBar />
+		</div>
+	);
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'))
