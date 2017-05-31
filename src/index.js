dc5ce22bd73208de0e126a051a54db84a2bc183e
diamantk <diamantkolshi@gmail.com>
2017-05-31 14:12:20 +0200

    add props at VideoList

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search'
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyDg-2GI3OThgY9UETmMHw5yXKF1w0HxY9U';
 
@@ -20,6 +21,7 @@ class App extends Component {
 		return (
 			<div>
 				<SearchBar />
+				<VideoList videos={this.state.videos} />
 			</div>
 		);
 	}
