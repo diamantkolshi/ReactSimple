f56cbf3b16d75227b5102c7b78fdb7669f225baa
diamantk <diamantkolshi@gmail.com>
2017-05-31 14:09:32 +0200

    remove input from serach-bar

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -10,9 +10,6 @@ class SearchBar extends Component {
 	render() {
 		return (
 			<div>
-				<input 
-					value = {this.state.term}
-					onChange={event => this.setState({ term: event.target.value })} />
 			</div>
 		);
 	}
