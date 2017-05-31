3fb75cd50d0a687f6ebc7e73d2d1c66c5e387f8e
diamantk <diamantkolshi@gmail.com>
2017-05-31 13:51:45 +0200

    add YTSerach to fetch youtube videos

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search'
 
 import SearchBar from './components/search_bar';
 // Create a new component. This component shuuld produce
 // some HTML
 const API_KEY = 'AIzaSyDg-2GI3OThgY9UETmMHw5yXKF1w0HxY9U';
+
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
+	// console.log(data);
+})
 
 const App = () =>  {
 	return (
